9b9cf78831c1dd9048ec8e05a14213ae94e869bd
Hayde Barbosa <haydebarbosam@gmail.com>
2019-07-02 00:56:37 -0500

    wow, react es genial!

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+	render(){
+		return(
+			<div>
+				<h1>Hello World!</h1>
+				<p>Yay! my first React Component!</p>
+			</div>
+		);
+	}
+}
+
+export default Hello;
