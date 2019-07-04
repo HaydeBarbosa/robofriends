61d0d7abd89c78bb3f7b66b39382a7844cf903c5
Hayde Barbosa <haydebarbosam@gmail.com>
2019-07-03 20:57:07 -0500

    React is so damn awesome, wow that was just mindblowing yet amazing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-//elements
+//components
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 
 //data
 import { robots } from './robots';
 
-const App = () => {
-	return (
-		<div className = "tc">
-			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList robots={robots}/>
-		</div>
-	);
+
+class App extends Component{
+
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchField: ''
+		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=>response.json())
+		.then(users=>this.setState({robots: users}));
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchField: event.target.value});
+	}
+
+
+	render(){
+
+		const filteredRobots = this.state.robots.filter( robot => {
+			return robot.name.toLowerCase().includes(this.state.searchField);
+		});
+
+		return (
+			<div className = "tc">
+				<h1>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+			</div>
+		);
+	}
+	
 
 }
 
